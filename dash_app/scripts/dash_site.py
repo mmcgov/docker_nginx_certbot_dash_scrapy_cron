90f8405dc88a713b5bcfd9cc8d65ca614788099c
mmcgov <mmcgov@outlook.com>
2020-08-02 23:58:11 +0100

    linter corrections

--- dash_app/scripts/dash_site.py
+++ dash_app/scripts/dash_site.py
@@ -4,13 +4,45 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_auth
-from flask import Flask
 import pandas as pd
-import datetime
 import plotly
 import plotly.graph_objs as go
 
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.config.suppress_callback_exceptions = True
+un_pw = [['user_1', 'pw_1'], ['un_2', 'pw_2'], ['un_3', 'pw_3']]
+auth = dash_auth.BasicAuth(app, un_pw)
+initial_country = 'Ireland'
+data = pd.read_csv('/home/data/graph_data.csv', index_col='date')
+intro = '''Interactive website analysing Covid-19 data.
+           The data source is https://www.worldometers.info/coronavirus/'''
+app.layout = html.Div(children=[html.H1(children='Covid 19 Data Analytics'),
+                                dcc.Markdown(children=intro),
+                                dcc.Dropdown(id='country',
+                                options=[{'label': i, 'value': i} for i in
+                                         list(set(data['country']))],
+                                         value='Ireland'),
+                                html.H4(id='live-update-text'),
+                                dcc.Graph(id='main_graph',
+                                          style={'width': 1000})
+                                ])
+
+
 def generate_table(dataframe, max_rows=10):
+    """
+    Function which generate dash table from dataframe
+
+    Parameters:
+    dataframe: pandas dataframe
+    max_rows (int): number of rows to show from dataframe
+
+    Returns
+    dash table showing requested number of rows from dataframe
+    """
+
     return html.Table(
         # Header
         [html.Tr([html.Th(col) for col in dataframe.columns])] +
@@ -22,66 +54,74 @@ def generate_table(dataframe, max_rows=10):
     )
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.config.suppress_callback_exceptions = True
-un_pw = [['user_1', 'pw_1'], ['un_2', 'pw_2'], ['un_3', 'pw_3']]
-auth = dash_auth.BasicAuth(app, un_pw)
-initial_country = 'Ireland'
-data  = pd.read_csv('/home/data/graph_data.csv', index_col='date')
-intro = '''Interactive website analysing Covid-19 data. The data source is https://www.worldometers.info/coronavirus/''' 
-app.layout = html.Div(children=[
-html.H1(children='Covid 19 Data Analytics'),
-dcc.Markdown(children=intro),
-dcc.Dropdown(id='country', options=[{'label': i, 'value': i} for i in list(set(data['country']))], value='Ireland'),
-html.H4(id='live-update-text'),
-dcc.Graph(id='main_graph',style={'width':1000})
-])
+@app.callback(Output('live-update-text', 'children'),
+              [Input('country', 'value')])
+def update_dataframe(value):
+    """
+    Dash function which updates data with chosen filter
 
-@app.callback(Output('live-update-text', 'children'), [Input('country', 'value')])
-def update_fund_value(value):
-    data  = pd.read_csv('/home/data/graph_data.csv')
-    data = data[data['country']==value]
-    a=data['date'].iloc[-1]
-    b=data['country'].iloc[-1]
-    c=data['total_cases'].iloc[-1]
-    d=round(data['case_growth_rate'].iloc[-1],2)
-    e=data['total_deaths'].iloc[-1]
-    summary_dict={'Date':[a], 'Country':[b], 'Total Cases':[c], 'Case growth rate':[f'{d}%'], 'Total Deaths':[e]}
+    Parameters:
+    value (str): chosen country to analyse
+    max_rows (int): number of rows to show from dataframe
+
+    Returns
+    dash table showing requested number of rows from filtered dataframe
+    """
+
+    data = pd.read_csv('/home/data/graph_data.csv')
+    data = data[data['country'] == value]
+    date = data['date'].iloc[-1]
+    country = data['country'].iloc[-1]
+    tot_cases = data['total_cases'].iloc[-1]
+    growth = round(data['case_growth_rate'].iloc[-1], 2)
+    deaths = data['total_deaths'].iloc[-1]
+    summary_dict = {'Date': [date],
+                    'Country': [country],
+                    'Total Cases': [tot_cases],
+                    'Case growth rate': [f'{growth}%'],
+                    'Total Deaths': [deaths]}
     summary_df = pd.DataFrame.from_dict(summary_dict)
     return generate_table(summary_df)
 
+
 @app.callback(Output('main_graph', 'figure'), [Input('country', 'value')])
-def update_layout(value):
+def update_graph(value):
+    """
+    Function which updates graph with chosen filter
+
+    Parameters:
+    value (str): chosen country to analyse
+
+    Returns
+    dash graph showing filtered data
+    """
+
     graph_df = pd.read_csv('/home/data/graph_data.csv')
-    graph_df = graph_df[graph_df['country']==value]
+    graph_df = graph_df[graph_df['country'] == value]
     # Create the graph with subplots
     fig = plotly.tools.make_subplots(specs=[[{'secondary_y': True}]])
-    fig['layout']['title'] = f'Cumulative total of Covid-19 cases in {value} along with daily rates of increase in new cases'
+    fig['layout']['title'] = f'Cumulative total of Covid-19 cases in {value}\
+                               along with daily rates of increase in new cases'
     fig['layout']['margin'] = {
         'l': 30, 'r': 10, 'b': 60, 't': 60
     }
     fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
-    
-    fig.add_trace(
-    go.Scatter(
-        x = graph_df['date'][-15:],
-        y = graph_df['total_cases'][-15:],
-        name = 'total cases'
-        ))
 
-    fig.add_trace(
-    go.Bar(
-        x = graph_df['date'][-15:],
-        y = graph_df['case_growth_rate'][-15:],
-        name = 'growth rate'),
-        secondary_y=True
-        )
+    fig.add_trace(go.Scatter(
+                              x=graph_df['date'][-15:],
+                              y=graph_df['total_cases'][-15:],
+                              name='total cases'
+                            ))
 
-    fig.show()    
+    fig.add_trace(go.Bar(
+                         x=graph_df['date'][-15:],
+                         y=graph_df['case_growth_rate'][-15:],
+                         name='growth rate'),
+                  secondary_y=True
+                  )
+
+    fig.show()
     return fig
-    
 
 
 if __name__ == '__main__':
--- scraper/scripts/scraper/scraper/spiders/scraper.py
+++ scraper/scripts/scraper/scraper/spiders/scraper.py
@@ -1,76 +1,96 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from collections import defaultdict
-#from scrapy.selector import HtmlXPathSelector
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 import pandas as pd
-import openpyxl
-import sys 
-import requests
-from lxml import html
 from datetime import datetime
-import time
-import os
-from datetime import date
-from scrapy.spiders import Spider
-from scrapy.http import FormRequest
-from scrapy.utils.response import open_in_browser
 from twisted.internet import reactor, defer
 from scrapy.crawler import CrawlerRunner
-from scrapy.crawler import Crawler
 from scrapy.utils.log import configure_logging
-from scrapy.utils.project import get_project_settings
-import ssl
-from functools import wraps
-import pandas as pd
-import os
-import requests
-import traceback
 import numpy as np
-from subprocess import call
+import os
 
 
 class CountriesItem(scrapy.Item):
-     country = scrapy.Field()
-     total_cases = scrapy.Field()
-     new_cases = scrapy.Field()
-     total_deaths = scrapy.Field()
-     new_deaths = scrapy.Field()
-     total_recovered = scrapy.Field()
-     active_cases = scrapy.Field()
-     serious_critical = scrapy.Field()
-     total_deaths_per_million_pop = scrapy.Field()
+    country = scrapy.Field()
+    total_cases = scrapy.Field()
+    new_cases = scrapy.Field()
+    total_deaths = scrapy.Field()
+    new_deaths = scrapy.Field()
+    total_recovered = scrapy.Field()
+    active_cases = scrapy.Field()
+    serious_critical = scrapy.Field()
+    total_deaths_per_million_pop = scrapy.Field()
 
 
 class scraper(scrapy.Spider):
-
-
     def __init__(self):
         name = 'coronavirus_tracker'
         allowed_domains = ['www.worldometers.info']
         start_urls = ['https://www.worldometers.info/coronavirus/#countries']
 
-
     def start_requests(self):
-            yield scrapy.Request('https://www.worldometers.info/coronavirus/#countries', callback=self.parse)
+        """
+        Function to start scraping with inital page url
 
+        Returns
+        response: url response which is then passed into the parse function
+        """
+
+        yield scrapy.Request('https://www.worldometers.info/coronavirus/#countries',
+                             callback=self.parse)
 
     def parse(self, response):
+        """
+        Function which does the scraping of the website including
+        collecting all the data frmo the table and saving it as
+        csv output files
+
+        Parameters:
+        response: url response which is then passed into the parse function
+
+
+        Returns
+        raw_res (list of mixed type): list of all the covid data on site
+        """
+
         global raw_res
         raw_res = list()
-        row = 8 
-        for _ in range(14,len(response.xpath('//*[@id="main_table_countries_today"]/tbody/tr'))-1):  
-            row += 1 
-            for column in range(2,16): 
-                try: 
-                    raw_res.append(response.xpath(f'//*[@id="main_table_countries_today"]/tbody[1]/tr[{row}]/td[{column}]//text()').extract_first()) 
-                except AttributeError: 
-                    raw_res.append(response.xpath(f'//*[@id="main_table_countries_today"]/tbody[1]/tr[{row}]/td[{column}]/text()').extract_first()) 
-         
- 
+        self.c_table = '//*[@id="main_table_countries_today"]/tbody/tr'
+        self.row = 8
+        for _ in range(14, len(response.xpath(self.c_table)) - 1):
+            self.row += 1
+            for self.column in range(2, 16):
+                self.row_data = f'//*[@id="main_table_countries_today"]/tbody[1]\
+                                  /tr[{self.row}]/td[{self.column}]//text()'
+                self.row_data_alt = f'//*[@id="main_table_countries_today"]/tbody[1]\
+                                     /tr[{self.row}]/td[{self.column}]/text()'
+                try:
+                    raw_res.append(
+                        response.xpath(f'//*[@id="main_table_countries_today"]\
+                            /tbody[1]/tr[{self.row}]/td[{self.column}]//text()'
+                                       ).extract_first())
+                except AttributeError:
+                    raw_res.append(
+                        response.xpath(f'//*[@id="main_table_countries_today"]\
+                            /tbody[1]/tr[{self.row}]/td[{self.column}]/text()'
+                                       ).extract_first())
+
     def closed(self, reason):
-        #appending new daily data to master data
+        """
+        Function which formats and cleans the data from the scrape
+        before saving it as csv files
+
+        Parameters:
+        reason: scrapy input means this function will only activate when
+        spider is finished
+
+        Returns
+        csv files: list of all the covid data on site
+        """
+        # appending new daily data to master data
+        self.case_data = '/home/data/covid_19_case_data.csv'
+        self.graph_data = '/home/data/graph_data.csv'
+        self.first_case_data = '/home/data/first_case.csv'
+        self.world_pop_data = '/home/data/world_pops.csv'
         columns = ['country',
                    'total_cases',
                    'new_cases',
@@ -85,41 +105,60 @@ class scraper(scrapy.Spider):
                    'total_tests_per_million_pop',
                    'population']
         today = datetime.now().strftime('%d/%m/%Y')
-        vals = zip(raw_res[0::14], raw_res[1::14], raw_res[2::14], raw_res[3::14], raw_res[4::14],
-           raw_res[5::14], raw_res[6::14], raw_res[8::14], raw_res[9::14], raw_res[10::14], 
-           raw_res[11::14], raw_res[12::14], raw_res[13::14])
-
-        results = pd.DataFrame(vals, columns= columns)
+        vals = zip(raw_res[0::14], raw_res[1::14], raw_res[2::14],
+                   raw_res[3::14], raw_res[4::14], raw_res[5::14],
+                   raw_res[6::14], raw_res[8::14], raw_res[9::14],
+                   raw_res[10::14], raw_res[11::14], raw_res[12::14],
+                   raw_res[13::14])
+        results = pd.DataFrame(vals, columns=columns)
         results.insert(0, 'date', today)
-        if os.path.isfile('/home/data/covid_19_case_data.csv'):
-            master_results = pd.read_csv('/home/data/covid_19_case_data.csv', index_col=False)
+
+        if os.path.isfile(self.case_data):
+            master_results = pd.read_csv(self.case_data,
+                                         index_col=False)
         else:
             master_results = pd.DataFrame()
         master_results = master_results.append(results)
-        master_results.to_csv('/home/data/covid_19_case_data.csv', index=False)
-        
+        master_results.to_csv(self.case_data, index=False)
+
         # Post formatting for graph data
         cols = list(master_results.columns[2:])
         master_results[cols] = master_results[cols].astype(str)
-        master_results['date'] = master_results['date'].apply(lambda x: pd.to_datetime(x, errors='coerce'))
-        master_results = master_results.sort_values(['country', 'date']).reset_index(drop=True)
-        master_results[cols] = master_results[cols].replace({r'^\s*$':0, 'None':0, 'nan':0, ',':'', 'N/A':0}, regex=True)
+        master_results['date'] = master_results['date'].apply(
+                                 lambda x: datetime.strptime(x, '%d/%m/%Y'))
+        master_results = master_results.sort_values(
+                         ['country', 'date']).reset_index(drop=True)
+        master_results[cols] = master_results[cols].replace(
+                               {r'^\s*$': 0, 'None': 0, 'nan': 0,
+                                ',': '', 'N/A': 0}, regex=True)
         master_results[cols] = master_results[cols].astype(float)
-        master_results['case_growth_rate'] = (master_results['new_cases']\
-                                              /master_results['total_cases'].shift(1))*100
-        master_results['case_growth_rate'] = np.where(master_results['case_growth_rate']<0,0,
-                                                      master_results['case_growth_rate'])
-        master_results['death_growth_rate'] = (master_results['new_deaths']\
-                                               /master_results['total_deaths'].shift(1))*100
-        master_results['death_growth_rate'] = np.where(master_results['death_growth_rate']<0,0,
-                                                       master_results['death_growth_rate'])
-        dates_df = pd.read_csv('/home/data/first_case.csv')
-        dates_dict = dict(zip(dates_df['country'],dates_df['date_first_case']))
-        master_results['first_case_date'] = master_results['country'].map(dates_dict)
+        master_results['case_growth_rate'] = (master_results['new_cases']
+                                              / master_results['total_cases']
+                                              .shift(1)) * 100
+        master_results['case_growth_rate'] = np.where(
+                                    master_results['case_growth_rate'] < 0, 0,
+                                    master_results['case_growth_rate'])
+        master_results['death_growth_rate'] = (master_results['new_deaths']
+                                               / master_results['total_deaths']
+                                               .shift(1)) * 100
+        master_results['death_growth_rate'] = np.where(
+                                    master_results['death_growth_rate'] < 0, 0,
+                                    master_results['death_growth_rate'])
+        pops_df = pd.read_csv(self.world_pop_data)
+        pops_dict = dict(zip(pops_df['country'],
+                             pops_df['pop_mills'].astype(float)))
+        master_results['pop_millions'] = master_results['country'].map(
+                                         pops_dict)
+        dates_df = pd.read_csv(self.first_case_data)
+        dates_dict = dict(zip(dates_df['country'],
+                              dates_df['date_first_case']))
+        master_results['first_case_date'] = master_results['country'].map(
+                                            dates_dict)
         master_results.fillna(0, inplace=True)
         for col in cols:
             master_results[col] = master_results[col].astype(int)
-        master_results.to_csv('/home/data/graph_data.csv', index=False)
+        master_results.to_csv((self.graph_data), index=False)
+
 
 configure_logging()
 runner = CrawlerRunner()
@@ -132,8 +171,9 @@ def crawl():
 
 
 def main():
-   crawl()
-   reactor.run() # the script will block here until the last crawl call is finished
+    crawl()
+    reactor.run()
+
 
 if __name__ == '__main__':
     main()
