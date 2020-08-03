a994a8e7018d2089ca6a9b54fd9ba9265110cb1f
mmcgov <mmcgov@outlook.com>
2020-08-03 19:05:47 +0100

    linter corrections

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=DOMAIN_NAME
+domains=DOMAIN_LESS_WWW
 rsa_key_size=4096
 data_path="./certbot"
 email="EMAIL_ADDRESS" # Adding a valid address is strongly recommended
